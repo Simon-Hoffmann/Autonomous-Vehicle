5c618b89715f815a3c9478f5ee7de9b8a022c291
Simon Hoffmann <63074808+Simon-Hoffmann@users.noreply.github.com>
2022-03-31 19:16:44 +0200

    eventhandler created

---
+++ Autonomous_RC_car/eventhandler.c
@@ -0,0 +1,48 @@
+/**************************************************************************
+ *  Autonom Eingebettete Systeme    (AEiSy)                               *  
+ *                                                                        *
+ **************************************************************************
+ *  PROJECT       MC Mobile  																							*
+ *  MODULE        eventhandler.c                                           		  *
+ *  REVISION      1.0                                                     *
+ *  AUTHOR        Simon Hoffmann & Aleksei Svatko            							*
+ **************************************************************************
+ *  PURPOSE:                                                              *
+ *                                      															    *
+ *                                                                        *
+ **************************************************************************
+ *  CHANGE HISTORY:                                                       *
+ *   Date  		       Author      						 Description       				    *
+ *   31.03.2022  		 S. Hoff & A. Svatko     creation 									  *
+ *                                                                        *
+ *************************************************************************/
+ 
+/* ---------------- G L O B A L   D E F I N I T I O N S ---------------- */
+
+/*-------------------------- I N C L U D E S ----------------------------*/
+
+#include "eventhandler.h"
+#include "led.h"
+#include "inithandler.h"
+
+/* ----------------- G L O B A L    V A R I A B L E S ------------------ */
+
+/* ------------  F U N C T I O N   D E F I N I T I O N ----------------- */
+
+void eventhandler_Handler1(EVENT_T curEvent){
+	switch(curEvent.EventID){
+		case EVT_NOEVENT:
+			break;
+		case EVT_INIT:
+			inithandler_initialise();
+			break;
+		case EVT_LED_ON:
+			led_on();
+			break;
+		case EVT_LED_OFF:
+			led_off();
+			break;
+		default:
+			break;
+	}
+}
---
+++ Autonomous_RC_car/eventhandler.h
@@ -0,0 +1,41 @@
+/**************************************************************************
+ *  Autonom Eingebettete Systeme    (AEiSy)                               *  
+ *                                                                        *
+ **************************************************************************
+ *  PROJECT       MC Mobile  																							*
+ *  MODULE        eventhandler.h                                           			  *
+ *  REVISION      1.0                                                     *
+ *  AUTHOR        Simon Hoffmann & Aleksei Svatko            							*
+ **************************************************************************
+ *  PURPOSE:                                                              *
+ *                                   																	    *
+ *                                                                        *
+ **************************************************************************
+ *  CHANGE HISTORY:                                                       *
+ *   Date  		       Author      						 Description       				    *
+ *   31.03.2022  		 S. Hoff & A. Svatko     creation              				*
+ *                                                                        *
+ *************************************************************************/
+
+#ifndef _EVENTHANDLER_H_
+#define _EVENTHANDLER_H_
+
+/*************************************************************************/
+/* -------------- E X P O R T E D   D E F I N I T I O N S -------------- */
+/*************************************************************************/
+
+#include "event.h"
+
+/*  - T y p e s                                                          */
+
+/*  - C o n s t a n t s                                                  */
+
+/*  - M a c r o s                                                        */
+
+/*  - P u p b l i c   V a r i a b l e s                                  */
+
+/*  - P u b l i c  F u n c t i o n  P r o t o t y p e s                 */
+
+void eventhandler_Handler1(EVENT_T curEvent);
+
+#endif
